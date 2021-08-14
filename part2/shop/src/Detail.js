927bb0f8db46fca29c3d4dcf590088bbefd93d58
colo1211 <colo1211@naver.com>
2021-08-15 02:14:38 +0900

    useHistory, goBack()

--- part2/shop/src/Detail.js
+++ part2/shop/src/Detail.js
@@ -16,7 +16,9 @@ function Detail(){
           <h4 className="pt-5">상품명</h4>
           <p>상품설명</p>
           <p>120000원</p>
-          <button className="btn btn-danger">뒤로가기</button> 
+          <button className="btn btn-danger" onClick = {()=>{
+            history.goBack(); 
+          }}>뒤로가기</button> 
           <button className="btn btn-danger">주문하기</button> 
         </div>
       </div>
