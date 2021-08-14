e24f73d859f5c7d4523f43965e39fe8ce6546d0a
colo1211 <colo1211@naver.com>
2021-08-14 17:14:16 +0900

    Detail Component

---
+++ part2/shop/src/Detail.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'; 
+import { useHistory } from 'react-router-dom'; 
+
+function Detail(){
+
+    let history = useHistory(); 
+    // 방문 기록을 저장해 놓는 Object
+
+    return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <img src="이미지파일/shoes1.jpg" width="100%" />
+        </div>
+        <div className="col-md-6 mt-4">
+          <h4 className="pt-5">상품명</h4>
+          <p>상품설명</p>
+          <p>120000원</p>
+          <button className="btn btn-danger">뒤로가기</button> 
+          <button className="btn btn-danger">주문하기</button> 
+        </div>
+      </div>
+</div> 
+    );
+}
+
+export default Detail; 
