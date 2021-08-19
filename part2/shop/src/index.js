5bdfa2b94ec282217c1f2ea3a0789e947c97d189
colo1211 <colo1211@naver.com>
2021-08-19 13:04:52 +0900

    Redux 데이터 수정 : reduce <-> dispatch({ type : ''}) 연습

--- part2/shop/src/index.js
+++ part2/shop/src/index.js
@@ -5,11 +5,44 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';  
+
+
+let 초기데이터 = [
+  {
+    name : 'White cute Shoes',
+    수량 : 1
+  },
+  {
+    name : 'Black Yark Shoes', 
+    수량 : 2
+  }
+];
+
+function reducer(state = 초기데이터, 액션){
+  if (액션.type === '수량증가'){
+    let temp = [...state];
+    temp[0].수량++; 
+    return temp; 
+  }else if (액션.type === '수량감소'){
+    let temp = [...state];
+    temp[0].수량--;
+    return temp; 
+  }
+  else {
+    return state;
+  }
+}
+
+let store = createStore(reducer); 
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
